c84fbca92ee68eba8224eb6b5154737514502c57
khadizajarin <khadiza131310@gmail.com>
2023-10-19 21:33:26 +0600

    cart added

--- src/componants/MyCart.jsx
+++ src/componants/MyCart.jsx
@@ -10,25 +10,46 @@ const MyCart = () => {
     
     useEffect(() => {
         const storeCartIds = getStoredProduct();
+
         if (myCart.length > 0) {
             const cart = [];
             for (const id of storeCartIds) {
-                const foundProduct = myCart.find((cartItem) => cartItem.id === id);
-                if (foundProduct) {
-                    cart.push(foundProduct);
-                }
+                const cartItem = myCart.find(cart => cart._id == id)
+                cart.push(cartItem);
             }
             setAdded(cart);
             setDisplayCart(cart);
-            console.log(storeCartIds, myCart);
-
+            console.log(storeCartIds, added,myCart);
         }
-    }, [myCart, added]);
+    }, [myCart]);
+
+
+
+
+
 
     return (
         <div>
             <Navbar></Navbar>
-            <h2>Cart</h2>
+            <div className="grid grid-cols-1 md:grid-cols-1 gap-4 max-w-7xl mx-auto mt-32">
+                {displayCart.map((added) => (
+                    <div key = {added._id} >
+                        <div className="card card-side bg-gray-500 ">
+                            <figure><img className="md:h-96 md:w-96" src={added.photo} alt={added.productName}/></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{added.brand}</h2>
+                                <p>{added.description}</p>
+                                <p>{added.price}</p>
+                                <p>{added.rating}</p>
+                                <div className="card-actions justify-end">
+                                <button className="btn btn-neutral">Delete from Cart</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
