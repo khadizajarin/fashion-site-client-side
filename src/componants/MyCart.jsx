138830756f223b7ac2c0aa85c3580e74156d92dd
khadizajarin <khadiza131310@gmail.com>
2023-10-20 02:17:35 +0600

    corrected cart add and display

--- src/componants/MyCart.jsx
+++ src/componants/MyCart.jsx
@@ -1,63 +1,81 @@
 import Navbar from "./Navbar";
-import { getStoredProduct} from "../utilities/uitlity";
-import { useLoaderData } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "./AuthProvide/AuthProvider";
 
 const MyCart = () => {
-    const myCart = useLoaderData();
+    // const myCart = useLoaderData();
+    const {user} = useContext(AuthContext);
 
-const [added, setAdded] = useState([]);
-const [displayCart, setDisplayCart] = useState([]);
+    const [added, setAdded] = useState([]);
+    // const [displayCart, setDisplayCart] = useState([]);
 
-useEffect(() => {
-  const storeCartIds = getStoredProduct();
+    // useEffect(() => {
+    // const storeCartIds = getStoredProduct();
 
-  if (myCart.length > 0) {
-    const cart = [];
-    for (const id of storeCartIds) {
-      const cartItem = myCart.find(cart => cart._id == id)
-      cart.push(cartItem);
-    }
-    setAdded(cart);
-    setDisplayCart(cart);
-  }
-}, [myCart]);
+    // if (myCart.length > 0) {
+    //     const cart = [];
+    //     for (const id of storeCartIds) {
+    //     const cartItem = myCart.find(cart => cart._id == id)
+    //     cart.push(cartItem);
+    //     }
+    //     setAdded(cart);
+    //     setDisplayCart(cart);
+    // }
+    // }, [myCart]);
 
-const handleCartDelete = (id) => {
-  const storedAllProduct = getStoredProduct();
+//     const handleCartDelete = (id) => {
+//     const storedAllProduct = getStoredProduct();
 
-  const index = storedAllProduct.indexOf(id);
+//     const index = storedAllProduct.indexOf(id);
 
-  if (index !== -1) {
-    storedAllProduct.splice(index, 1);
-    localStorage.setItem('addedCart', JSON.stringify(storedAllProduct));
+//     if (index !== -1) {
+//         storedAllProduct.splice(index, 1);
+//         localStorage.setItem('addedCart', JSON.stringify(storedAllProduct));
 
-    // Update the added state without a full route reload
-    const remaining = added.filter(item => item._id !== id);
-    console.log(remaining);
-    setAdded(remaining);
-    setDisplayCart(remaining);
-  }
-}
+//         // Update the added state without a full route reload
+//         const remaining = added.filter(item => item._id !== id);
+//         console.log(remaining);
+//         setAdded(remaining);
+//         setDisplayCart(remaining);
+//   }
+//     }
 
+    useEffect(() => {
+        fetch('http://localhost:5000/cart', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            const cartFound = data.filter(product => product.email=== user.email);
+            setAdded(cartFound);
+            console.log(user, added);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }, [added]);
 
 
     return (
         <div>
             <Navbar></Navbar>
             <div className="grid grid-cols-1 md:grid-cols-1 gap-4 max-w-7xl mx-auto mt-32">
-                {displayCart.map((added) => (
-                    <div key = {added._id} >
+                {added.map((add) => (
+                    <div key = {add._id} >
                         <div className="card lg:card-side bg-base-100 shadow-xl">
-                            <figure><img className="lg:h-96 lg:w-96" src={added.photo}  alt={added.productName}/></figure>
+                            <figure><img className="lg:h-96 lg:w-96" src={add.object.photo}  alt={add.object.productName}/></figure>
                             <div className="card-body">
-                                <h2 className="card-title text-3xl">{added.productName}</h2>
-                                <p className="text-xl font-bold">{added.type}</p>
-                                <p className="text-lg">{added.description}</p>
-                                <p>{added.price}</p>
-                                <p>{added.rating}</p>
+                                <h2 className="card-title text-3xl">{add.object.productName}</h2>
+                                <p className="text-xl font-bold">{add.object.type}</p>
+                                <p className="text-lg">{add.object.description}</p>
+                                <p>{add.object.price}</p>
+                                <p>{add.object.rating}</p>
                                 <div className="card-actions justify-end">
-                                <button onClick={() => handleCartDelete(added._id)} className="btn btn-neutral">Delete from Cart</button>
+                                <button className="btn btn-neutral">Delete from Cart</button>
                                 </div>
                             </div>
                         </div>
--- src/componants/ProductsDetails/Products.jsx
+++ src/componants/ProductsDetails/Products.jsx
@@ -2,26 +2,48 @@ import{ useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import Navbar from "../Navbar";
 import Advertisement from "../Advertisement";
-import { saveProduct } from "../../utilities/uitlity";
+// import Swal from "sweetalert2";
+import { useContext} from "react";
+import { AuthContext } from "../AuthProvide/AuthProvider";
 import Swal from "sweetalert2";
 
 const Products = (params) => {
   const [products, setProducts] = useState([]);
   const { name } = useParams();
 
+  const {user} = useContext(AuthContext);
 
-  const handleCartAdd =(id) => {
-    console.log(id);
-    saveProduct(id);
-    Swal.fire({
-      icon: "success",
-      title: `Product added to you cart successfully!`,
-      text: `See your cart List here in My Cart!`,
-      timer: 2000
+//  add product to cart
+    const handleCartAdd= ((user,object) => {
+      const email = user.email;
+      const newCart ={email , object}
+      console.log(newCart);
+    
+      fetch('http://localhost:5000/cart',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newCart)
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if(data.inserted){
+          Swal.fire({
+            icon: "success",
+            title: 'Product Added to Cart Successfully!',
+            timer: 2000
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
       });
-    }
- 
+    });
 
+
+    // add new product to to database
   useEffect(() => {
     fetch('https://fashion-site-server-go9vg31hf-khadizajarin.vercel.app/addProduct', {
       method: 'GET',
@@ -46,13 +68,11 @@ const Products = (params) => {
       <Advertisement></Advertisement>
 
       {/* displaying the name */}
-
       
         <h1 className="text-xl text-center">
           Displaying products for brand: <strong>{name}</strong>
         </h1>
-      
-                
+                 
       {   /* products showing */}
 
       <div className="max-w-7xl mx-auto mt-20">
@@ -72,7 +92,7 @@ const Products = (params) => {
                           <div className="card-actions justify-end">
                             <button className="btn btn-neutral">Details</button>
                             <Link to={`/updateProduct/${product._id}`}><button className="btn btn-neutral">Update</button></Link>
-                            <button onClick={() => handleCartAdd(product._id)}  className="btn btn-neutral">Add to Cart</button>
+                            <button onClick={() => handleCartAdd(user,product)}  className="btn btn-neutral">Add to Cart</button>
                           </div>
                         </div>
                     </div>
